140dc45d5610db5208b5884cbda8982552ab7abf
yangshuchao <yangshuchao@ppsports.com>
2017-09-18 19:15:08 +0800

    init

---
+++ app/src/main/java/io/github/robinvanyang/qrcodeextract/App.java
@@ -0,0 +1,16 @@
+package io.github.robinvanyang.qrcodeextract;
+
+import android.app.Application;
+
+/**
+ * TODO
+ *
+ * @author RobinVanYang created at 2017-09-18 11:50.
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
